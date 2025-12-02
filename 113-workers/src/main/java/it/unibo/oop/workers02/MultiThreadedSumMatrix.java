b5ffcce757bae410950d50380beb2c57d6b380b1
Diego Montalti <diego.montalti@studio.unibo.it>
2025-12-02 18:25:40 +0100

    Finishing all exercise at home. Exercise complete

---
+++ 113-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,104 @@
+package it.unibo.oop.workers02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    private final int nthread;
+
+    /**
+     * Builds a multithreaded list sum.
+     *
+     * @param nthread
+     *            no. of thread performing the sum.
+     */
+    public MultiThreadedSumMatrix(final int nthread) {
+        this.nthread = nthread;
+    }
+
+    @Override
+    public double sum(double[][] matrix) {
+
+        final int rows = matrix.length;
+        final int colums = matrix[0].length;
+        final int dimension = rows * colums;
+        final int size = dimension % nthread + dimension / nthread;
+        /*
+         * Build a list of workers
+         */
+        final List<Worker> workers = new ArrayList<>(nthread);
+        for (int start = 0; start < dimension; start += size) {
+            workers.add(new Worker(matrix, start, size));
+        }
+        /*
+         * Start them
+         */
+        for (final Worker w: workers) {
+            w.start();
+        }
+        /*
+         * Wait for every one of them to finish. This operation is _way_ better done by
+         * using barriers and latches, and the whole operation would be better done with
+         * futures.
+         */
+        long sum = 0;
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (final InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        /*
+         * Return the sum
+         */
+        return sum;
+    }
+
+    private static class Worker extends Thread {
+        private final double[][] matrix;
+        private final int startpos;
+        private final int nelem;
+        private long res;
+
+        /**
+         * Build a new worker.
+         *
+         * @param list
+         *            the list to sum
+         * @param startpos
+         *            the initial position for this worker
+         * @param nelem
+         *            the no. of elems to sum up for this worker
+         */
+        Worker(final double[][] matrix, final int startpos, final int nelem) {
+            super();
+            this.matrix = matrix;
+            this.startpos = startpos;
+            this.nelem = nelem;
+        }
+
+        @Override
+        public synchronized void run() {
+            final int rows = matrix.length;
+            final int colums = matrix[0].length;
+            final int dimension = rows * colums;
+            // Println used to show the working ranges for debugging purposes
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1)); // NOPMD
+            for (int i = startpos; i < dimension && i < startpos + nelem; i++) {
+                this.res += this.matrix[i/colums][i%colums];
+            }
+        }
+
+        /**
+         * Returns the result of summing up the integers within the list.
+         *
+         * @return the sum of every element in the array
+         */
+        public synchronized long getResult() {
+            return this.res;
+        }
+    }
+}
